d063fd95437d69e0ffb4ce9f9baed8fa0faff9ac
NguyenChinh0207 <nchinh407@gmail.com>
2020-12-14 16:57:19 +0700

    update Java Extra_assign1

---
+++ Java/Extra_Assignment1/src/extra_assignment1/HeSinhVien.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extra_assignment1;
+
+/**
+ *
+ * @author May tinh Duy Toi
+ */
+public class HeSinhVien {
+    BacHoc bacHoc;
+    
+}
+enum BacHoc{
+DAI_HOC, CAO_DANG
+}
---
+++ Java/Extra_Assignment1/src/extra_assignment1/Student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extra_assignment1;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author May tinh Duy Toi
+ */
+public class Student {
+    int id;
+    String ten;
+    LocalDate tuoi;
+    String queQuan;
+    HeSinhVien heSinhVien;
+}
